bc4a1d75a5006663c88232727a89e104506f2332
Otto Pilot <otto.40000ft@gmail.com>
2020-04-13 23:21:33 -0500

    marked-proc: Add bottle unneeded

--- Formula/marked-proc.rb
+++ Formula/marked-proc.rb
@@ -4,6 +4,8 @@ class MarkedProc < Formula
   url "https://github.com/40000ft/marked-proc/archive/v1.0.1.tar.gz"
   sha256 "0a41665f14d70ea8e3eef1a3034b833161525533bb5b2b6138138893380c9cab"
 
+  bottle :unneeded
+
   def install
     bin.install "marked-proc"
   end
