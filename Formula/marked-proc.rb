8aa774e3154c284409094fe9824b864715faca21
Otto Pilot <otto.40000ft@gmail.com>
2021-09-08 16:02:48 -0500

    Update marked-proc to v1.1.0

--- Formula/marked-proc.rb
+++ Formula/marked-proc.rb
@@ -1,8 +1,8 @@
 class MarkedProc < Formula
   desc "Custom processor for the Marked2 App"
   homepage "https://github.com/40000ft/marked-proc"
-  url "https://github.com/40000ft/marked-proc/archive/v1.0.1.tar.gz"
-  sha256 "0a41665f14d70ea8e3eef1a3034b833161525533bb5b2b6138138893380c9cab"
+  url "https://github.com/40000ft/marked-proc/archive/v1.1.0.tar.gz"
+  sha256 "ba0119a7d6dd3463b8d237a4adff3013fce6cb10adf49099e23a8d3440a66465"
 
   bottle :unneeded
 
