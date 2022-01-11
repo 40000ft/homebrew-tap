43c91aa0133dd047be5cf5d30d6f2b233997c29e
Otto Pilot <otto.40000ft@gmail.com>
2022-01-11 14:09:13 -0600

    marked-proc.rb: Remove deprecated bottle:unneeded

--- Formula/marked-proc.rb
+++ Formula/marked-proc.rb
@@ -4,8 +4,6 @@ class MarkedProc < Formula
   url "https://github.com/40000ft/marked-proc/archive/v1.1.0.tar.gz"
   sha256 "ba0119a7d6dd3463b8d237a4adff3013fce6cb10adf49099e23a8d3440a66465"
 
-  bottle :unneeded
-
   def install
     bin.install "marked-proc"
   end
